5370a2e89172c06439d172e0ad6966a7fee74330
shouriemansi <mansi.shourie24@gmail.com>
2026-07-27 12:35:38 +0530

    Add maxium of all the elements

---
+++ 06-2DArray/maximum.java
@@ -0,0 +1,32 @@
+//Find the maximum element
+import java.util.Scanner;
+public class maximum {
+    public static void main(String[] args){
+        Scanner inp=new Scanner(System.in);
+        System.out.print("Enter number of rows and columns: ");
+        int n=inp.nextInt();
+        int arr[][]=new int[n][n];
+        System.out.println("Enter elements: ");
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                arr[i][j]=inp.nextInt();
+            }
+        }
+        System.out.println("You entered elements: ");
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+        int max=arr[0][0];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(arr[i][j]>max){
+                    max=arr[i][j];
+                }
+            }
+        }
+        System.out.println("Maximum element: " +  max);
+    }
+}
