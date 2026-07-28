43be2f75d9b8a6eebd747ceda14e5ae63908f2ee
shouriemansi <mansi.shourie24@gmail.com>
2026-07-28 21:19:41 +0530

    Add data types program

---
+++ 01-Basics/dataTypes.java
@@ -0,0 +1,29 @@
+public class dataTypes {
+    public static void main(String[] args){
+        // Integer data types
+        byte byteValue = 100;
+        short shortValue = 30000;
+        int intValue = 100000;
+        long longValue = 10000000000L;
+
+        // Decimal data types
+        float floatValue = 3.14f;
+        double doubleValue = 3.1415926535;
+
+        // Character data type
+        char grade = 'A';
+
+        // Boolean data type
+        boolean isJavaFun = true;
+
+        // Displaying all primitive data types
+        System.out.println("byte    : " + byteValue);
+        System.out.println("short   : " + shortValue);
+        System.out.println("int     : " + intValue);
+        System.out.println("long    : " + longValue);
+        System.out.println("float   : " + floatValue);
+        System.out.println("double  : " + doubleValue);
+        System.out.println("char    : " + grade);
+        System.out.println("boolean : " + isJavaFun);
+    }
+}
