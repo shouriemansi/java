15f3cb68495282e7c68e41a939ee00d3bca3b8ae
shouriemansi <mansi.shourie24@gmail.com>
2026-07-28 21:17:49 +0530

    Add variables program

---
+++ 01-Basics/variables.java
@@ -0,0 +1,25 @@
+public class variables {
+    public static void main(String[] args){
+        // Integer
+        int age = 19;
+
+        // Decimal number
+        double cgpa = 9.2;
+
+        // Single character
+        char grade = 'A';
+
+        // True or False
+        boolean isStudent = true;
+
+        // Text/String
+        String name = "Shyam";
+
+        // Printing variables
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("CGPA: " + cgpa);
+        System.out.println("Grade: " + grade);
+        System.out.println("Student: " + isStudent);
+    }    
+}
