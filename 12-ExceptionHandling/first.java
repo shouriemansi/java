6482c0fb4eb69295cfc4e486afda735694cdf761
shouriemansi <mansi.shourie24@gmail.com>
2026-08-31 10:31:59 +0530

    added one more program for null pointer exception

--- 12-ExceptionHandling/first.java
+++ 12-ExceptionHandling/first.java
@@ -1,19 +1,38 @@
 //Write a Java program that: Takes two integers from the user. Divides the first number by the second. Handles ArithmeticException if the second number is 0.
 
+// import java.util.*;
+// public class first {
+//     public static void main(String[] args) {
+//         Scanner inp=new Scanner(System.in);
+//         System.out.print("Enter first number: ");
+//         int n1=inp.nextInt();
+//         System.out.print("Enter second number: ");
+//         int n2=inp.nextInt();
+//         try{
+//             int result=n1/n2;
+//             System.out.println("Division result: " + result);
+//         }
+//         catch(ArithmeticException e){
+//             System.out.println("Cannot divide by 0");
+//         }
+//     }
+// }
+
+
+
+//Null String: Create a String variable and initially assign: String name = null;
+// Try to call: name.length(); Handle the exception.
+
 import java.util.*;
 public class first {
     public static void main(String[] args) {
         Scanner inp=new Scanner(System.in);
-        System.out.print("Enter first number: ");
-        int n1=inp.nextInt();
-        System.out.print("Enter second number: ");
-        int n2=inp.nextInt();
+        String str=null;
         try{
-            int result=n1/n2;
-            System.out.println("Division result: " + result);
+            System.out.println("Size: " + str.length());
         }
-        catch(ArithmeticException e){
-            System.out.println("Cannot divide by 0");
+        catch(NullPointerException e){
+            System.out.println("Null pointer exception!");
         }
     }
 }
